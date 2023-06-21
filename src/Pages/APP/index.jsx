ef7f6358cd127665f684a1839b829cc97e7c0425
BryanGaray <bryangarayacademico@gmail.com>
2023-06-20 23:06:33 -0500

    Creación del contexto Shopping Cart

--- src/Pages/APP/index.jsx
+++ src/Pages/APP/index.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, useRoutes } from 'react-router-dom';
+import { ShoppingCartProvider } from '../../Context';
 import Home from '../Home';
 import MyAccount from '../MyAccount';
 import MyOrder from '../MyOrder';
@@ -23,10 +24,13 @@ const AppRoutes = () => {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-      <Navbar />
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <AppRoutes />
+        <Navbar />
+      </BrowserRouter>  
+    </ShoppingCartProvider>
+    
   )
 }
 
